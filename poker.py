3e261420290137a7127000ee65e4b6da49f58c7b
Rhamsez Thevenin <rhamzthev@gmail.com>
2020-12-29 12:10:44 -0500

    initial push. I just made a card class, making queue for deck of cards in the future.

---
+++ poker.py
@@ -0,0 +1,51 @@
+class Card:
+    __slots__ = ["__name", "__rank", "__suit"]
+
+    def __init__(self, rank, suit):
+        self.__rank = rank
+        self.__suit = suit
+        self.__name = self.make_name(rank, suit)
+
+    def make_name(self, rank, suit):
+        a_rank = rank
+
+        if a_rank == 11:
+            a_rank = "Jack"
+        elif a_rank == 12:
+            a_rank = "Queen"
+        elif a_rank == 13:
+            a_rank = "King"
+        elif a_rank == 14:
+            a_rank = "Ace"
+
+        return f"{a_rank} of {suit}"
+
+    def face(a_rank):
+        if a_rank == 11:
+            return "Jack"
+        elif a_rank == 12:
+            return "Queen"
+        elif a_rank == 13:
+            return "King"
+        elif a_rank == 14:
+            return "Ace"
+    
+    def rank(self):
+        return self.__rank
+
+    def suit(self):
+        return self.__suit
+
+    def __str__(self):
+        return self.__name
+
+class Queue:
+    __slots__ = []
+
+# Make a deck of Cards using a List Queue 
+
+def main():
+    card = Card(13, "Hearts")
+    print(card)
+
+if __name__ == "__main__": main()
---
+++ poker_main.py
@@ -0,0 +1,7 @@
+import poker
+
+def main():
+    card = poker.Card(13, "Hearts")
+    print(card)
+
+if __name__ == "__main__": main()
