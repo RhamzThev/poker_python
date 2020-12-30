17beb80dddc7ceb9d157cd1c29471dedaff790fd
Rhamsez Thevenin <rhamzthev@gmail.com>
2020-12-30 15:00:10 -0500

    just made queue class for deck. will make attempts to create game next time.

--- poker.py
+++ poker.py
@@ -1,3 +1,9 @@
+import random
+
+RED = "\033[31m"
+BLACK = "\033[34m"
+RESET = "\033[0m"
+
 class Card:
     __slots__ = ["__name", "__rank", "__suit"]
 
@@ -20,26 +26,74 @@ class Card:
 
         return f"{a_rank} of {suit}"
 
-    def face(a_rank):
-        if a_rank == 11:
-            return "Jack"
-        elif a_rank == 12:
-            return "Queen"
-        elif a_rank == 13:
-            return "King"
-        elif a_rank == 14:
-            return "Ace"
-    
     def rank(self):
         return self.__rank
 
     def suit(self):
         return self.__suit
 
-    def __str__(self):
+    def __repr__(self):
         return self.__name
 
-class Queue:
-    __slots__ = []
+    def __str__(self):
+        if self.__rank == 10:
+            a_rank = 10
+        elif self.__rank > 10:
+            a_rank = self.__name[0]
+        else:
+            a_rank = self.__rank 
+        
+        a_suit = self.__suit[0]
 
-# Make a deck of Cards using a List Queue 
+        if a_suit == "C" or a_suit == "S":
+            card = BLACK + str(a_rank) + a_suit + RESET
+            return card
+        elif a_suit == "D" or a_suit == "H":
+            card = RED + str(a_rank) + a_suit + RESET
+            return card
+
+class Queue:
+    slots = ["__list"]
+
+    def __init__(self):
+        self.__list = []
+
+    def next(self):
+        try:
+            if len(self.__list) > 0:
+                return self.__list[-1]
+            else:
+                raise IndexError
+        except IndexError:
+            print("Can't do that. Queue is empty.")
+
+    def enqueue(self, item):
+        self.__list.append(item)
+
+    def dequeue(self):
+        try:
+            if len(self.__list) > 0:
+                item = self.__list.pop(0)
+                return item
+            else:
+                raise IndexError
+        except IndexError:
+            print("Can't do that. Queue is empty.")
+
+    def length(self):
+        return len(self.__list)
+
+    def shuffle(self):
+        random.shuffle(self.__list)
+
+    def __str__(self):
+        string = ""
+        for item in self.__list:
+            string += str(item) + " "
+        return string.strip()
+        
+RANKS = [i for i in range(2, 15)]
+
+SUITS = ["Clubs", "Diamonds", "Hearts", "Spades"]
+
+# Next step: Apply steps to make poker game happen.
--- poker_main.py
+++ poker_main.py
@@ -1,7 +1,14 @@
-import poker
+from poker import *
 
 def main():
-    card = poker.Card(13, "Hearts")
-    print(card)
+    deck = Queue()
+    
+    for suit in SUITS:
+        for rank in RANKS:
+            deck.enqueue(Card(rank, suit))
+    
+    deck.shuffle()
+    print(deck)
+    print(deck.length())
 
 if __name__ == "__main__": main()
